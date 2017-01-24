58ea3aef407e860dee417229bf01fcefe07d60a0
iingemar <iingemar@gmail.com>
2017-01-24 10:42:50 +0100

    Add Bleep model form

--- app_name/admin.py
+++ app_name/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
-
+from .forms import BleepModelForm
 from .models import Bleep
 
-admin.site.register(Bleep)
+
+class BleepModelAdmin(admin.ModelAdmin):
+    form = BleepModelForm
+
+admin.site.register(Bleep, BleepModelAdmin)
---
+++ app_name/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Bleep
+
+
+class BleepModelForm(forms.ModelForm):
+    class Meta:
+        model = Bleep
+        fields = [
+            'user',
+            'content'
+        ]
+
+    def clean_content(self, *args, **kwargs):
+        content = self.cleaned_data.get('content')
+        if content == 'abc':
+            raise forms.ValidationError('Cannot be abc')
+        return content
