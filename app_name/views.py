c277621bb0907c40bcb61d442ed79a732ba3c6c8
iingemar <iingemar@gmail.com>
2017-01-25 15:07:02 +0100

    Move check for authenticated user to mixin

--- app_name/views.py
+++ app_name/views.py
@@ -1,26 +1,16 @@
-from django import forms
-from django.forms.utils import ErrorList
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView
 
 from .forms import BleepModelForm
+from .mixins import FormUserNeededMixin
 from .models import Bleep
 
 
-class BleepCreateView(CreateView):
+class BleepCreateView(FormUserNeededMixin, CreateView):
     form_class = BleepModelForm
     template_name = 'bleep/bleep_create_view.html'
     success_url = '/bleeps/create'
 
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        if self.request.user.is_authenticated():
-            form.instance.user = self.request.user
-            return super(BleepCreateView, self).form_valid(form)
-        else:
-            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(['User must be logged in.'])
-            return self.form_invalid(form)
 
 def bleep_create_view(request):
     # Populate form
