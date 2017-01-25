e6d9216b4d8b371bcfb079d210c3cd3f2d1ea44c
iingemar <iingemar@gmail.com>
2017-01-25 15:02:24 +0100

    Add check for authenticated user

--- app_name/urls.py
+++ app_name/urls.py
@@ -8,6 +8,6 @@ from .views import (
 
 urlpatterns = [
     url(r'^$', index_view, name='index_view'),
-    url(r'^create/$', bleep_create_view, name='bleep_create_view'),
+    url(r'^create/$', BleepCreateView.as_view(), name='bleep_create_view'),
     url(r'^(?P<bleep_id>[0-9]+)$', bleep_detail_view, name='bleep_detail_view'),
 ]
--- app_name/views.py
+++ app_name/views.py
@@ -1,3 +1,5 @@
+from django import forms
+from django.forms.utils import ErrorList
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView
 
@@ -13,9 +15,12 @@ class BleepCreateView(CreateView):
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        form.instance.user = self.request.user
-        return super(BleepCreateView, self).form_valid(form)
-
+        if self.request.user.is_authenticated():
+            form.instance.user = self.request.user
+            return super(BleepCreateView, self).form_valid(form)
+        else:
+            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(['User must be logged in.'])
+            return self.form_invalid(form)
 
 def bleep_create_view(request):
     # Populate form
