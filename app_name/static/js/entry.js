099daeca1cf74e72fe179ac7eee04528574618e0
iingemar <iingemar@gmail.com>
2017-02-01 15:25:27 +0100

    Add ajax Bleep list view

--- app_name/static/js/entry.js
+++ app_name/static/js/entry.js
@@ -26,3 +26,26 @@ var IndexView = Backbone.View.extend({
 var indexView = new IndexView();
 
 indexView.render();
+
+$(document).ready(function() {
+    console.log('ajax');
+
+    $.ajax({
+        url: '/api/bleeps',
+        method: 'GET',
+        success: function(bleeps) {
+            console.log('SUCCESS');
+            console.log(bleeps);
+            _.each(bleeps, function(bleep){
+                console.log(bleep);
+                console.log(bleep.content);
+                $('.bleep-container').append('<li>' + bleep.content + '</li>');
+            })
+        },
+        error: function(bleeps) {
+            console.log('ERROR');
+            console.log(bleeps);
+        }
+
+    });
+});
