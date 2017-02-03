f42272edc6c2f6595c7b85df2f35b636f308d99a
iingemar <iingemar@gmail.com>
2017-02-03 13:58:18 +0100

    Add user profile with followers

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    # One user has one profile
+    # Accessed by user object.profile
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
+
+    # We can follow a lot of users and a lot of users can follow us.
+    # user.profile.following -- users I follow
+    # related_name is the reverse relationship: user.followed_by -- users that follow me
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
+
+    def __str__(self):
+        return self.user.username + ' (' + str(self.following.all().count()) + ')'
